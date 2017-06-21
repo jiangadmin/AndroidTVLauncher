5e8988ec574e5194a540d1cae568376ef1013205
jiangadmin <www.fangmu@qq.com>
2017-06-21 22:09:57 +0800

    列表名

--- CompatibleLauncher/src/main/java/com/jiang/launcher/Entity/Const.java
+++ CompatibleLauncher/src/main/java/com/jiang/launcher/Entity/Const.java
@@ -10,4 +10,30 @@ package com.jiang.launcher.Entity;
  */
 public class Const {
     public static String URL="";
+
+    public static String MangoTV="com.hunantv.license";//芒果TV
+//    public static String SuiXinTing="com.xgimi.duobanfm";//随心听
+//    public static String SuiXinTing="com.xgimi.duobanfm";//沙发管家
+//    public static String ZhiBo="com.hd.zhibo";//超级直播
+//
+//    public static String HDP="hdpfans.com";//HDP直播
+//    public static String HDP="com.hitutu.albumsxkxgimi";//杰兔画报极米版
+//    public static String HDP="com.android.providers.downl";//下载
+//    public static String HDP="com.gimigame";//游戏
+//
+//    public static String HDP="com.xgimi.instrucation";//电子说明书
+//    public static String HDP="com.jd.smart.tv.sdk";//京东微联
+//    public static String HDP="com.android.setting";//系统设置
+//    public static String HDP="mstar.factorymenu.ui";//Factory Menu
+//
+//    public static String HDP="mstar.tvsetting.ui";//Tvsetting
+//    public static String HDP="com.xgimi.screensaver.main";//屏保
+//    public static String HDP="cn.wps.moffice_i18n_TV";//WPS投影宝
+//    public static String HDP="com.xgimi.wasutv";//影视资源
+
+
+
+
+
+
 }
